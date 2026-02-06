590ed294c60a88feb20d26f129b501595284f7aa
Impala4c <mansuradan9@gmail.com>
2026-02-06 14:17:15 +0500

    urls

--- backend/complaint_system/urls.py
+++ backend/complaint_system/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('',include('feedback.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('feedback.urls')),
     path('api/auth/token/', obtain_auth_token),
